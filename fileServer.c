5a2eb780f8f8955a8e07194760b53d2c8f8553fb
Dylan Kernohan <kernohad@mail.gvsu.edu>
2018-09-25 14:42:24 -0400

    renamed thread1 to thread_id. Cleaned up comments

--- fileServer.c
+++ fileServer.c
@@ -12,18 +12,27 @@ void sigHandler(int sigNum);
 const int INPUT_SIZE = 50;  // number of characters user can input
 int fileCounter = 0;        // Used to keep track of the number of files accessed.
 
+
+
+/*****************************************************
+ * A Fileserver simulation. The initial thread prompts
+ * the user for input, then spawns a worker thread
+ * and sends the input (filename) to the worker to
+ * so it can "find" it. 
+ ****************************************************/
 int main(){
     char input[INPUT_SIZE];
 
-    pthread_t thread1;  // Might need to dynamically make these if we cannot reuse it. TODO 
+    pthread_t thread_id;  // Might need to dynamically make these if we cannot reuse it. TODO 
     int status;
 
-    // Override ^C signal
+    /* Override ^C signal */
     signal(SIGINT, sigHandler);
 
-    // Loop through the following 2 steps until user enters in ^C signal, then gracefully shutdown: TODO
+    /* Loop through the following 2 steps until user enters in ^C signal, then gracefully shutdown: TODO */
     while(1){
-        // Prompt user for filename string
+        
+        /* Prompt user for filename string */
         printf("\n> ");
         fgets(input, INPUT_SIZE, stdin);
        
@@ -31,40 +40,40 @@ int main(){
         printf("Filename: %s\n", input);
 
         
-        // Spawn child thread tell it filename string
-        if (( status = pthread_create (&thread1, NULL, worker_code, &input)) != 0) {
+        /* Spawn child thread tell it filename string */
+        if (( status = pthread_create (&thread_id, NULL, worker_code, &input)) != 0) {
                 fprintf (stderr, "thread create error %d: %s\n", status, strerror(status));
                 exit(1);
         }
     }
 
-    // TODO: Remove this, just to test passing filename to thread
-    sleep(11);
-
     return 0;
 }
 
+
+/*****************************************************
+ * Function passed to the child threads for them to run
+ ****************************************************/
 void* worker_code (void* arg){
     // TODO: clean up print statements in sleep ifs
 
     char input[INPUT_SIZE];
-    strcpy(input, arg);
-    
-    srand(time(0));
+    strcpy(input, arg);                     // Copy the argument passed to the function to the input var
+    srand(time(0));                         // Seed Rand with the time
     int sleepChance = rand() % 5 + 1;       // Random number between 1 and 5
     
-    fileCounter += 1;   // Increment the number of files accessed.
-
-    // 80% probability, sleep 1 sec. 
-    // The simulates the worker thread found the desired file in the cache and serves it up quickly 
+    fileCounter += 1;                       // Increment the number of files accessed.
+ 
+     /* 80% probability, sleep 1 sec. 
+     * The simulates the worker thread found the desired file in the cache and serves it up quickly */
     if( sleepChance < 5 ){
         printf("Sleep Num: %d\n", sleepChance);
         printf("Sleep for 1 second.\n");
         sleep(1);
     }
 
-    // 20% probability sleep 7-10 deconds (randomly).
-    // This simulates the worker has not found the requested file in cache and must block while it reads from the disk
+    /* 20% probability sleep 7-10 deconds (randomly).
+     * This simulates the worker has not found the requested file in cache and must block while it reads from the disk */
     if( sleepChance == 5 ){
         printf("Sleep Num: %d\n", sleepChance);
         int sleepTime = (rand() % (10 - 7)) + 7;     // Generate random number between 7 and 10
@@ -73,12 +82,15 @@ void* worker_code (void* arg){
     }
 
     // TODO: Remove this, just to test passing filename to thread
-    printf("Thread Filename: %s\n", input);
+    printf("(%ld)Thread Filename: %s\n", pthread_self(), input);
 
 
     return arg;
 }
 
+/*****************************************************
+ * Function used to override the ^C signal
+ ****************************************************/
 void sigHandler(int sigNum){
     printf(" recieved.\nThe number of files accessed is %d.\nShutting down now.\n", fileCounter);
     exit(0);
